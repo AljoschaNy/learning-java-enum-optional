ec33c8c9e5d2fc38da891113728c049fcb636602
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-06 11:06:07 +0200

    Add varibales weekday and weekdayType and getter for both

--- src/main/java/org/example/DaysOfWeek.java
+++ src/main/java/org/example/DaysOfWeek.java
@@ -1,5 +1,27 @@
 package org.example;
 
 public enum DaysOfWeek {
-    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+    MONDAY("monday", "working day"),
+    TUESDAY("tuesday", "working day"),
+    WEDNESDAY("wednesday", "working day"),
+    THURSDAY("thursday", "working day"),
+    FRIDAY("friday", "working day"),
+    SATURDAY("saturday", "weekend"),
+    SUNDAY("sunday", "weekend");
+
+    private final String weekday;
+    private final String weekdayType;
+
+    DaysOfWeek(String weekday, String weekDayType) {
+        this.weekday = weekday;
+        this.weekdayType = weekDayType;
+    }
+
+    public String getWeekday() {
+        return weekday;
+    }
+
+    public String getWeekDayType() {
+        return weekdayType;
+    }
 }
